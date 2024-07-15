a7c7f7f40a5a67b9b2c368f463607b2d63423db1
João Pi <pinela@gmail.com>
2024-07-15 17:37:20 +0100

    Update variables.tf

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -3,29 +3,3 @@ variable "region" {
   default     = "us-west-2"
 }
 
-variable "cluster_name" {
-  description = "The name of the EMR cluster."
-  default     = "emr-07"
-}
-
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket."
-  default     = "emr-07-01-ecu32"
-}
-
-variable "emr_role_name" {
-  description = "The name of the IAM role."
-  default     = "emr-07-role"
-}
-
-variable "number_of_nodes" {
-  description = "The number of nodes in the EMR cluster core."
-  default     = 1
-}
-
-variable "subnet_id" {
-  description = "The ID of the subnet to launch the EMR cluster in."
-  default ="subnet_null"
-}
-
-
